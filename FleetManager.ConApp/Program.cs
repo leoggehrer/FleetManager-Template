cf034419d3686e2047f2bb13aa070ab5f4707252
Gehrer Gerhard <56598504+leoggehrer@users.noreply.github.com>
2024-11-19 12:06:03 +0100

    Fixed unit tests.

--- FleetManager.ConApp/Program.cs
+++ FleetManager.ConApp/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Der Flottenmanager");
+            Console.WriteLine();
+
+            Fleet fleet = new Fleet(100_000);
+            fleet.AddVehicle(new PassengerVehicle("123456789X", 2000, 30, 10.0));
+            fleet.AddVehicle(new CargoVehicle("349913599X", 5000, 10, 50.0));
+            fleet.AddVehicle(new PassengerVehicle("0747551006", 2500, 32, 10.0));
+            fleet.AddVehicle(new CargoVehicle("1572314222", 3000, 10, 50.0));
+
+            Console.WriteLine("Fleet.ToString()");
+            Console.WriteLine($"{fleet}");
+
+            Console.WriteLine();
+            Console.WriteLine("Fleet.GetByRevenue()");
+            foreach (var vehicle in fleet.GetByRevenue())
+            {
+                Console.WriteLine($"{vehicle}");
+            }
         }
     }
 }
